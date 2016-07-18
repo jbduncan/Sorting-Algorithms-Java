aa502c6f28d04c8ceb5bb8a16b01439f4846bbfb
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2016-07-18 16:58:25 +0100

    HeapSort cleanup

--- Sort Algorithms/src/uk/co/bluettduncanj/serial/HeapSort.java
+++ Sort Algorithms/src/uk/co/bluettduncanj/serial/HeapSort.java
@@ -9,49 +9,48 @@ import java.util.List;
 
 
 /**
- * @author Jonathan
+ * @author Jonathan Bluett-Duncan
  */
-public class HeapSort<T extends Comparable<T>> {
+public final class HeapSort<T extends Comparable<T>> {
   
   private HeapSort() {}
 
-  private class Heap<Item extends Comparable<Item>> {
-    ArrayList<Item> heap = new ArrayList<Item>();
+  private static final class Heap<Item extends Comparable<Item>> {
+    private List<Item> heap = new ArrayList<Item>();
 
     public void add(Item item) {
       int index = heap.size();
       heap.add(item);
-      while (index > 0 && item.compareTo(heap.get((index - 1)/2)) < 0){
-        heap.set(index, heap.get((index-1)/2));
-        index = (index-1) / 2;
+      while (index > 0 && item.compareTo(heap.get((index - 1) / 2)) < 0){
+        heap.set(index, heap.get((index - 1) / 2));
+        index = (index - 1) / 2;
       }
       heap.set(index, item);
     }
 
     public Item remove() {
       Item ans = heap.get(0);
-      Item sift = heap.remove(heap.size()-1);
-      if (heap.isEmpty()){
+      Item sift = heap.remove(heap.size() - 1);
+      if (heap.isEmpty()) {
         return ans;
       }
       else {
         int index = 0;
-        while ((2*index+2) < heap.size() && 
-                (sift.compareTo(heap.get(2*index + 1)) > 0 
-                    || sift.compareTo(heap.get(2*index + 2)) > 0)) {
-          if (heap.get(2*index + 1).compareTo(heap.get(2*index+2)) < 0) {
-            heap.set(index, heap.get(2*index+1));
-            index = 2*index + 1;
-          }
-          else {
-            heap.set(index, heap.get(2*index+2));
-            index = 2*index+2;        
+        while ((2 * index + 2) < heap.size() && 
+                (sift.compareTo(heap.get(2 * index + 1)) > 0 
+                    || sift.compareTo(heap.get(2 * index + 2)) > 0)) {
+          if (heap.get(2 * index + 1).compareTo(heap.get(2 * index + 2)) < 0) {
+            heap.set(index, heap.get(2 * index + 1));
+            index = 2 * index + 1;
+          } else {
+            heap.set(index, heap.get(2 * index + 2));
+            index = 2 * index + 2;        
           }
         }
-        if (2*index+2 == heap.size()){
-          if (sift.compareTo(heap.get(2*index+1)) > 0) {
-            heap.set(index, heap.get(2*index+1));
-            index = 2*index+1;
+        if (2 * index + 2 == heap.size()) {
+          if (sift.compareTo(heap.get(2 * index + 1)) > 0) {
+            heap.set(index, heap.get(2 * index + 1));
+            index = 2 * index + 1;
           }
         }
         heap.set(index, sift);
@@ -78,10 +77,10 @@ public class HeapSort<T extends Comparable<T>> {
    * This method does not return a separate copy of the list. Therefore, if you need separate copies, e.g.
    * one ordered and one unordered, do that before calling this method on one of the copies.
    * 
-   * @param list: The Java list of Comparables to sort. 
+   * @param list the list of Comparables to sort
    */
   public void sortAsc(List<T> list) {
-    HeapSort<T>.Heap<T> heap = new HeapSort<T>().new Heap<T>();
+    HeapSort.Heap<T> heap = new HeapSort.Heap<T>();
     while (!list.isEmpty()) {
       heap.add(list.remove(0));
     }
